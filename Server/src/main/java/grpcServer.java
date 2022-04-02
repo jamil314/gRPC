927f1e9110378224658cffbb8740cd133f0f371e
Jamil <jamil31415926@gmail.com>
2022-04-03 01:42:12 +0600

    Add files via upload

---
+++ Server/src/main/java/grpcServer.java
@@ -0,0 +1,25 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import services.DBHandler;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class grpcServer {
+
+    private static final Logger logger = Logger.getLogger(grpcServer.class.getName());
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Server server = ServerBuilder
+                .forPort(8080)
+                .addService(new DBHandler())
+                .build();
+        server.start();
+
+        logger.info("Server started at port : " + server.getPort());
+        server.awaitTermination(120, TimeUnit.SECONDS);
+    }
+}
